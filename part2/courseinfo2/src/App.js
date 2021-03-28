28096d398ba6451612997650a7d655dba280eb93
adriencyberspace <60076088+adriencyberspace@users.noreply.github.com>
2021-03-28 11:55:39 -0700

    2.5 courseinfo2 completion

--- part2/courseinfo2/src/App.js
+++ part2/courseinfo2/src/App.js
@@ -1,30 +1,7 @@
 import React from 'react'
+import Course from './components/Course'
 
-const Course = (props) => {
 
-  const result = props.courses.map(course =>
-    <div key={course.id}>
-      <h1>{course.name}</h1>
-      {course.parts.map(part =>
-        <p key={part.id}>{part.name} {part.exercises}</p>
-      )}
-      <Sum course={course} />
-    </div>
-  )
-
-  return result
-}
-
-const Sum = (props) => {
-
-  const array = props.course.parts.map(part => part.exercises)
-  const reducer = (accumulator, currentValue) => accumulator + currentValue
-  const sum = array.reduce(reducer)
-  
-  return (
-    <p><strong>Total of {sum} exercises</strong></p>
-  )
-}
 
 const App = () => {
   const courses = [
