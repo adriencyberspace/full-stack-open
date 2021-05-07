4890ca41814ba9369be845caf76452c0abe86428
adriencyberspace <60076088+adriencyberspace@users.noreply.github.com>
2021-05-07 09:49:51 -0700

    4.8

---
+++ part4/tests/blog_api.test.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+
+const api = supertest(app)
+
+test('blog posts are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('there are two blog posts', async () => {
+  const response = await api.get('/api/blogs')
+
+  expect(response.body).toHaveLength(2)
+})
+
+test('the first blog post title is by Adrien', async () => {
+  const response = await api.get('/api/blogs')
+
+  expect(response.body[0].author).toBe('Adrien')
+})
+
+afterAll(() => {
+  mongoose.connection.close()
+})
