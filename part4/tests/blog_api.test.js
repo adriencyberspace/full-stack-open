12882419657bc78a6f665ae1a6f24bd50f8b573f
adriencyberspace <60076088+adriencyberspace@users.noreply.github.com>
2021-05-07 10:59:09 -0700

    4.10 - needs work

--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
+
 const app = require('../app')
 
 const api = supertest(app)
@@ -20,13 +21,39 @@ test('there are two blog posts', async () => {
 test('the first blog post title is by Adrien', async () => {
   const response = await api.get('/api/blogs')
 
-  expect(response.body[0].author).toBe('Adrien')
+  expect(response.body[0].author).toBe('Sparky')
 })
 
 test('unique identifier is named id', async () => {
   const response = await api.get('/api/blogs')
 
   expect(response.body[0].id).toBeDefined()
+})
+
+test('a valid blog post can be added ', async () => {
+  const blogs = await api.get('/api/blogs')
+
+
+  const newBlog = {
+    title: 'I Pee When You Leave',
+    author: "Abby",
+    url: "https://www.google.com",
+    likes: 666
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+  const blogsAtEnd = await api.get('/api/blogs')
+  expect(blogsAtEnd.body).toHaveLength(blogs.body.length + 1)
+
+  // const contents = notesAtEnd.map(n => n.content)
+  // expect(contents).toContain(
+  //   'async/await simplifies making async calls'
+  // )
 })
 
 
