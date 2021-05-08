ff790879602ac673ff0dae9fd084e5261ebafac9
adriencyberspace <60076088+adriencyberspace@users.noreply.github.com>
2021-05-08 06:47:24 -0700

    4.11 and 4.12

--- part4/app.js
+++ part4/app.js
@@ -1,9 +1,11 @@
 const config = require('./utils/config')
 const express = require('express')
+require('express-async-errors')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 const blogsRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 require('dotenv').config()
 
@@ -15,7 +17,11 @@ mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, us
 app.use(cors())
 app.use(express.static('build'))
 app.use(express.json())
+app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -23,11 +23,13 @@ test('blog posts are returned as json', async () => {
     .expect('Content-Type', /application\/json/)
 })
 
+// 4.8
 test('there are two blog posts', async () => {
   const response = await api.get('/api/blogs')
 
   expect(response.body).toHaveLength(2)
 })
+
 
 test('the first blog post title is by Adrien', async () => {
   const response = await api.get('/api/blogs')
@@ -35,12 +37,14 @@ test('the first blog post title is by Adrien', async () => {
   expect(response.body[0].author).toBe('Sparky')
 })
 
+// 4.9
 test('unique identifier is named id', async () => {
   const response = await api.get('/api/blogs')
 
   expect(response.body[0].id).toBeDefined()
 })
 
+// 4.10
 test('a valid blog post can be added ', async () => {
   const newBlog = {
     title: 'I Pee When You Leave',
@@ -58,10 +62,46 @@ test('a valid blog post can be added ', async () => {
   const blogsAtEnd = await helper.blogsInDb()
   expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
 
-  // const contents = notesAtEnd.map(n => n.content)
-  // expect(contents).toContain(
-  //   'async/await simplifies making async calls'
-  // )
+})
+
+// 4.11
+test('if no likes, set likes to 0', async () => {
+  const newBlog = {
+    title: 'I Pee When You Leave',
+    author: "Abby",
+    url: "https://www.google.com"
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+  const blogsAtEnd = await helper.blogsInDb()
+
+  const x = helper.initialBlogs.length
+
+  expect(blogsAtEnd).toHaveLength(x+1)
+  expect(blogsAtEnd[x].likes).toBe(0)
+
+})
+
+// 4.12
+test('missing title or url returns 400 error', async () => {
+  const newBlog = {
+    author: "Abby",
+    likes: 5
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(400)
+
+  const blogsAtEnd = await helper.blogsInDb()
+
+  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
 })
 
 
