0ddae633934665ced9846b7e02b26ee7b8bd944d
adriencyberspace <60076088+adriencyberspace@users.noreply.github.com>
2021-05-22 14:36:58 -0700

    6.6

--- part6/unicafe-redux/src/App.js
+++ part6/unicafe-redux/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react'
-import Anecdotes from './components/Anecdotes'
-import NewAnecdote from './components/NewAnecdote'
+import AnecdoteList from './components/AnecdoteList'
+import AnecdoteForm from './components/AnecdoteForm'
 
 const App = () => {
 
   return (
     <div>
-      <NewAnecdote />
-      <Anecdotes />
+      <AnecdoteForm />
+      <AnecdoteList />
     </div>
   )
 }
