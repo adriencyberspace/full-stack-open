ea5a7fc528d37e801e490eb7b17b7fc18b50bbfc
adriencyberspace <60076088+adriencyberspace@users.noreply.github.com>
2021-05-23 20:09:09 -0700

    correct project

---
+++ part6/redux-anecdotes2/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import AnecdoteList from './components/AnecdoteList'
+import AnecdoteForm from './components/AnecdoteForm'
+
+const App = () => {
+
+  return (
+    <div>
+      <AnecdoteForm />
+      <AnecdoteList />
+    </div>
+  )
+}
+
+export default App
