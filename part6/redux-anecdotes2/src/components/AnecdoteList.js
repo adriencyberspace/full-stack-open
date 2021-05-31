6cd82a0450b26a694c8d1d6f54743330e9e34168
adriencyberspace <60076088+adriencyberspace@users.noreply.github.com>
2021-05-31 15:10:12 -0700

    6.12

--- part6/redux-anecdotes2/src/components/AnecdoteList.js
+++ part6/redux-anecdotes2/src/components/AnecdoteList.js
@@ -17,7 +17,14 @@ const Anecdote = ({ anecdote, handleClick }) => {
 const AnecdoteList = () => {
   const dispatch = useDispatch()
 
-  const anecdotes = useSelector(state => state.anecdotes)
+  const anecdotes = useSelector(state => {
+    const filter = state.filter
+    const anecdotes = state.anecdotes
+    if ( filter === '') {
+      return anecdotes
+    }
+    return anecdotes.filter(anecdote => anecdote.content.toLowerCase().includes(`${filter}`))
+  })
 
   const vote = (anecdote) => {
     dispatch(addVoteTo(anecdote.id))
