63e22555695d1f77edd53e2b57b712af126975bb
adriencyberspace <60076088+adriencyberspace@users.noreply.github.com>
2021-06-02 17:04:55 -0700

    7.5

---
+++ part7/routed-anecdotes/src/components/CreateNew.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import  { useField } from '../hooks'
+
+const CreateNew = (props) => {
+  const content = useField('text')
+  const author = useField('text')
+  const info = useField('text')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    props.addNew({
+      content: content.value,
+      author: author.value,
+      info: info.value,
+      votes: 0,
+    })
+    handleReset(e)
+  }
+
+  const handleReset = (e) => {
+    e.preventDefault()
+    content.reset()
+    author.reset()
+    info.reset()
+  }
+
+  return (
+    <div>
+      <h2>create a new anecdote</h2>
+      <form id="anecdote-form">
+        <div>
+          content
+          <input {...content}/>
+        </div>
+        <div>
+          author
+          <input {...author}/>
+        </div>
+        <div>
+          url for more info
+          <input {...info}/>
+        </div>
+        <button onClick={handleSubmit}>create</button>
+        <button onClick={handleReset}>reset</button>
+      </form>
+    </div>
+  )
+
+}
+
+export default CreateNew
