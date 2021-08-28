12c889fd044a89e5b11aefca97546a5ad428fedc
adriencyberspace <60076088+adriencyberspace@users.noreply.github.com>
2021-08-28 13:53:34 -0700

    remove git ignore

--- part2/countries/src/components/Error.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const Error = () => {
-  return <div>Too many matches. Please specify another filter.</div>
-}
-
-export default Error
--- part7/bloglist-keep/src/store.js
+++
@@ -1,7 +0,0 @@
-import { createStore } from 'redux'
-
-import notificationReducer from './reducers/notificationReducer'
-
-const store = createStore(notificationReducer)
-
-export default store
