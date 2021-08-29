ec8a2885b532d5d393fc380c005edb091e85b883
adriencyberspace <60076088+adriencyberspace@users.noreply.github.com>
2021-08-29 12:49:35 -0700

    8.4

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -116,7 +116,7 @@ const typeDefs = gql`
   type Query {
     authorCount: Int!
     bookCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -126,7 +126,13 @@ const resolvers = {
   Query: {
     authorCount: () => authors.length,
     bookCount: () => books.length, 
-    allBooks: () => books,
+    allBooks: (root, args) => {
+      if (!args.author) {
+        return books
+      }
+
+      return books.filter(b => b.author === args.author)
+    },
     allAuthors: () => {
       // Object to store author names and their book counts
       const authorsBookCount = authors.map((author) =>
