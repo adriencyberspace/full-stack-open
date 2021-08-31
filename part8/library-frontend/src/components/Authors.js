11184146373ea8f5bd8cce3150af3394b9511f67
adriencyberspace <60076088+adriencyberspace@users.noreply.github.com>
2021-08-30 21:43:26 -0700

    8.11 before refactor

--- part8/library-frontend/src/components/Authors.js
+++ part8/library-frontend/src/components/Authors.js
@@ -1,7 +1,47 @@
   
-import React from 'react'
-import { useQuery } from '@apollo/client'
-import { ALL_AUTHORS } from '../queries'
+import React, { useState } from 'react'
+import { useQuery, useMutation } from '@apollo/client'
+import { ALL_AUTHORS, EDIT_AUTHOR } from '../queries'
+
+const AuthorForm = () => {
+  const [editAuthor] = useMutation(EDIT_AUTHOR, {
+    refetchQueries: [{ query: ALL_AUTHORS}],
+  })
+
+  const [name, setName] = useState('')
+  const [born, setBorn] = useState('')
+
+  const submit = async (event) => {
+    event.preventDefault()
+
+    editAuthor({ variables: { name, setBornTo: parseInt(born) } })
+
+    setName('')
+    setBorn('')
+  }
+
+  return (
+    <div>
+      <h2>Set Birthyear</h2>
+
+      <form onSubmit={submit}>
+        <div>
+          Author Name <input
+            value={name}
+            onChange={({ target }) => setName(target.value)}
+          />
+        </div>
+        <div>
+          Born <input
+            value={born}
+            onChange={({ target }) => setBorn(target.value)}
+          />
+        </div>
+        <button type='submit'>Update Author</button>
+      </form>
+    </div>
+  )
+}
 
 const Authors = (props) => {
   const { loading, error, data } = useQuery(ALL_AUTHORS);
@@ -38,7 +78,7 @@ const Authors = (props) => {
           )}
         </tbody>
       </table>
-
+      <AuthorForm />
     </div>
   )
 }
--- part8/library-frontend/src/queries.js
+++ part8/library-frontend/src/queries.js
@@ -41,3 +41,16 @@ mutation addBook(
 }
 `
 
+export const EDIT_AUTHOR = gql`
+  mutation editAuthor(
+    $name: String!, 
+    $setBornTo: Int!) {
+    editAuthor(
+      name: $name, 
+      setBornTo: $setBornTo
+    )  {
+      name
+      born
+    }
+  }
+`
