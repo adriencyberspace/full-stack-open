eeaac91eb71941c9665b43eb37308482b81d89e7
adriencyberspace <60076088+adriencyberspace@users.noreply.github.com>
2021-08-30 18:28:08 -0700

    8.8

---
+++ part8/library-frontend/src/components/Authors.js
@@ -0,0 +1,46 @@
+  
+import React from 'react'
+import { useQuery } from '@apollo/client'
+import { ALL_AUTHORS } from '../queries'
+
+const Authors = (props) => {
+  const { loading, error, data } = useQuery(ALL_AUTHORS);
+  if (loading) return 'Loading...';
+  if (error) return `Error! ${error.message}`;
+
+  if (!props.show) {
+    return null
+  }
+
+  // This needs to go below the loading handler
+  const authors = data.allAuthors
+
+  return (
+    <div>
+      <h2>authors</h2>
+      <table>
+        <tbody>
+          <tr>
+            <th></th>
+            <th>
+              born
+            </th>
+            <th>
+              books
+            </th>
+          </tr>
+          {authors.map(a =>
+            <tr key={a.name}>
+              <td>{a.name}</td>
+              <td>{a.born}</td>
+              <td>{a.bookCount}</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+
+    </div>
+  )
+}
+
+export default Authors
