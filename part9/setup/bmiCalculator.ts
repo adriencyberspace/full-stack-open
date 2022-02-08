99e4f8420e5dfd3a838bc52ab11e4381b4c3f1f3
adriencyberspace <60076088+adriencyberspace@users.noreply.github.com>
2022-02-08 14:32:46 -0600

    9.1

---
+++ part9/setup/bmiCalculator.ts
@@ -0,0 +1,16 @@
+const calculateBmi = (height: number, weight: number): string => {
+  const bmi = (weight/((height*height)/100))*100
+
+  if (bmi < 18.5){
+    return "Underweight (unhealthy weight)"
+  } else if (bmi >= 18.5 && bmi < 24.9){
+    return "Normal (healthy weight)"
+  } else if (bmi >= 24.9 && bmi < 29.9){
+    return "Overweight (unhealthy weight)"
+  } else {
+    return "Obese (unhealthy weight)"
+  }
+
+}
+
+console.log(calculateBmi(180, 50))
